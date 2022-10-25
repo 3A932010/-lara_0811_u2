d4ff77654473ff1a694ff32db973ee530a3b986a
3A932010 <jack37699646@gmail.com>
2022-10-25 16:56:59 +0800

    練習7將Route的內容搬至Controller內

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -61,4 +62,5 @@ Route::group(['prefix' => 'admin'], function(){
     });
 });
 
-
+//
+Route::get('home', [HomeController::class, 'index'])->name('home.index');
