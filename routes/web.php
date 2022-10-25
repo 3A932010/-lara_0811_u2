71de0d271c176af63905b43dd71d368850b5eebb
3A932010 <jack37699646@gmail.com>
2022-10-25 10:22:12 +0800

    練習一設定Route回應view

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,7 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+//設定Route回應view
 Route::get('/', function () {
     return view('welcome');
 });
